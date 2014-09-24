df0ece600f4e198c66ad51ee87152a145fa1a767
llDavidll <d.lericolais@gmail.com>
2014-09-24 19:08:02 +0200

    Project skeleton with simple hello world.

---
+++ app/src/main/java/smartring/masterihm/enac/com/smartdring/HomeActivity.java
@@ -0,0 +1,14 @@
+package smartring.masterihm.enac.com.smartdring;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+public class HomeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
