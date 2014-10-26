0fdb0d81ce13150b52d0c60d5e62bd94c97e06a7
arnaud33200 <arnaud33200@gmail.com>
2014-10-26 13:39:15 +0100

    #16 appel vers liste contact fonctionnel mais impossible de récupé la requete

--- app/src/main/java/smartring/masterihm/enac/com/smartdring/fragments/ActionWhiteListFragment.java
+++ app/src/main/java/smartring/masterihm/enac/com/smartdring/fragments/ActionWhiteListFragment.java
@@ -1,11 +1,19 @@
 package smartring.masterihm.enac.com.smartdring.fragments;
 
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import smartring.masterihm.enac.com.smartdring.R;
 import smartring.masterihm.enac.com.smartdring.data.Profile;
@@ -16,7 +24,8 @@ import smartring.masterihm.enac.com.smartdring.data.Profile;
 public class ActionWhiteListFragment extends Fragment {
 
     public static final String TAG = "ActionWhiteListeFragmentTag";
-    //private static final String PROFILE_KEY = "Whilte_List";
+    private TextView nameText;
+    public static final int PICK_CONTACT_REQUEST = 1;
 
     public static ActionWhiteListFragment getInstance() {
 
@@ -31,7 +40,29 @@ public class ActionWhiteListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View profilesView = inflater.inflate(R.layout.fragment_action_whitelist, container, false);
+        nameText = (TextView) profilesView.findViewById(R.id.phoneText);
+        View button = profilesView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nameText.setText("TiTi");
+                Intent contactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+                contactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+                startActivityForResult(contactIntent, PICK_CONTACT_REQUEST);
+            }
+        });
         return profilesView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_CONTACT_REQUEST){
+            nameText.setText("TOTO");
+            if (resultCode == Activity.RESULT_OK) {
+                String name = data.getStringExtra("name");
+                nameText.setText(name);
+            }
+        }
     }
 }
 
