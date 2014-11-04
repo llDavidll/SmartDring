9b672297f080af118eb8ec335238b21700dc8afb
arnaud33200 <arnaud33200@gmail.com>
2014-11-04 12:33:44 +0100

    #19 Volume gérer depuis la fenetre d'edition de profile

--- app/src/main/java/smartring/masterihm/enac/com/smartdring/fragments/ProfileEditionFragment.java
+++ app/src/main/java/smartring/masterihm/enac/com/smartdring/fragments/ProfileEditionFragment.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.media.AudioManager;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import smartring.masterihm.enac.com.smartdring.R;
 import smartring.masterihm.enac.com.smartdring.SmartDringActivity;
@@ -24,7 +27,12 @@ public class ProfileEditionFragment extends Fragment {
     public final static String TAG = "ProfileEditionFragmentTag";
     private static final String PROFILE_KEY = "PROFILE_KEY";
 
-    public AudioManager am;
+    private AudioManager am;
+    private SeekBar soundLevelPhone;
+    private SeekBar soundLevelNotif;
+    private SeekBar soundLevelMedia;
+    private SeekBar soundLevelCall;
+    private SeekBar soundLevelAlarm;
 
     public static ProfileEditionFragment getInstance(@Nullable Profile profile) {
 
@@ -41,6 +49,98 @@ public class ProfileEditionFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_profile_edition, container, false);
         FragmentActivity fa = SmartDringActivity.mainActivity;
         am = (AudioManager)fa.getSystemService(Context.AUDIO_SERVICE);
+
+        final TextView txt = (TextView) v.findViewById(R.id.soundLevelTxt);
+
+        soundLevelPhone = (SeekBar) v.findViewById(R.id.soundLevelPhone);
+        soundLevelPhone.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int scale = i * am.getStreamMaxVolume(AudioManager.STREAM_SYSTEM) / 100;
+                am.setStreamVolume(AudioManager.STREAM_SYSTEM,scale,0);
+                String r = "System : " + am.getStreamVolume(AudioManager.STREAM_SYSTEM);
+                txt.setText(r);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        soundLevelNotif = (SeekBar) v.findViewById(R.id.soundLevelNotif);
+        soundLevelNotif.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int scale = i * am.getStreamMaxVolume(AudioManager.STREAM_NOTIFICATION) / 100;
+                am.setStreamVolume(AudioManager.STREAM_NOTIFICATION,scale,0);
+                String r = "Notif : " + am.getStreamVolume(AudioManager.STREAM_NOTIFICATION);
+                txt.setText(r);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        soundLevelMedia = (SeekBar) v.findViewById(R.id.soundLevelMedia);
+        soundLevelMedia.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int scale = i * am.getStreamMaxVolume(AudioManager.STREAM_MUSIC) / 100;
+                am.setStreamVolume(AudioManager.STREAM_MUSIC,scale,0);
+                String r = "Media: " + am.getStreamVolume(AudioManager.STREAM_MUSIC);
+                txt.setText(r);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        soundLevelCall = (SeekBar) v.findViewById(R.id.soundLevelCall);
+        soundLevelCall.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int scale = i * am.getStreamMaxVolume(AudioManager.STREAM_RING) / 100;
+                am.setStreamVolume(AudioManager.STREAM_RING,scale,0);
+                String r = "CALL: " + am.getStreamVolume(AudioManager.STREAM_RING);
+                txt.setText(r);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        soundLevelAlarm = (SeekBar) v.findViewById(R.id.soundLevelAlarm);
+        soundLevelAlarm.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int scale = i * am.getStreamMaxVolume(AudioManager.STREAM_ALARM) / 100;
+                am.setStreamVolume(AudioManager.STREAM_ALARM,scale,0);
+                String r = "Alarm: " + am.getStreamVolume(AudioManager.STREAM_ALARM);
+                txt.setText(r);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
         return v;
     }
 }
